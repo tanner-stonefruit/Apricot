28468ee8867f1247939d7e09ab314500e067e1a7
tanneruzzell <uzzelltanner@gmail.com>
2025-09-04 20:36:52 -0700

    Initial Commit

---
+++ Apricot/ApricotApp.swift
@@ -0,0 +1,17 @@
+//
+//  ApricotApp.swift
+//  Apricot
+//
+//  Created by Tanner Uzzell on 9/4/25.
+//
+
+import SwiftUI
+
+@main
+struct ApricotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Apricot/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Apricot
+//
+//  Created by Tanner Uzzell on 9/4/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
